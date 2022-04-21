c6ac5d9940bc21b3d8ef8978a3a92560bf79405b
player01osu <player01osu@gmail.com>
2022-04-21 14:39:41 -0400

    fix crash

--- dwm.c
+++ dwm.c
@@ -1432,9 +1432,10 @@ void monocle(Monitor *m)
 	for (c = m->clients; c; c = c->next)
 		if (ISVISIBLE(c))
 			n++;
-	for (c = nexttiled(m->clients); c; c = nexttiled(c->next))
+	for (c = nexttiled(m->clients); c; c = nexttiled(c->next)) {
 		resize(c, m->wx, m->wy, m->ww - 2 * c->bw, m->wh - 2 * c->bw,
 		       0);
+	}
 }
 
 void motionnotify(XEvent *e)
