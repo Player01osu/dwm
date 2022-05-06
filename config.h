bbdbf677423e1d254545dfe7384ae1863e0c9b2a
player01osu <player01osu@gmail.com>
2022-05-06 07:22:32 -0400

    osu and wine classes set to floating

--- config.h
+++ config.h
@@ -57,6 +57,8 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class       instance       title         tags mask     isfloating   monitor */
+	{ "osu!.exe",  NULL,          NULL,         0,             1,          -1 },
+	{ "wine",      NULL,          NULL,         0,             1,          -1 },
 	{ "Gimp",      NULL,          NULL,         0,             1,          -1 },
 	{ "Alacritty", NULL,          "pulsemixer", 0,             1,          -1 },
 	{ "Firefox",   NULL,          NULL,         1 << 8,        0,          -1 },
