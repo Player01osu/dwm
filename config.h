dcd2c964bd753d68edb8bdb15c0050a47944d479
player01osu <player01osu@gmail.com>
2022-05-22 19:56:29 -0400

    decrease outer gap, and bar height

--- config.h
+++ config.h
@@ -5,14 +5,14 @@ static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 11;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 11;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 13;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 13;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappoh    = 11;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 11;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int horizpadbar        = 3;        /* horizontal padding for statusbar */
 static const int vertpadbar         = 10;        /* vertical padding for statusbar */
-static const int user_bh            = 27;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
+static const int user_bh            = 26;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const char *fonts[]          = { "Source Code Pro Bold:size=8:style=Bold:antialias=true:autohint=true",
                                         "Font Awesome 6 Free:size=10:style=Regular:antialias=true:autohint=true",
                                         "Font Awesome 6 Free:size=10:style=Solid:antialias=true:autohint=true",
